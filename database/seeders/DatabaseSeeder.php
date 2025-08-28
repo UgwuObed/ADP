f77405e7ee9dd1f2a7a78c7e9b5d0633777774a3
UgwuObed <vjazzyvictor@gmail.com>
2025-08-28 20:59:30 +0100

    fest

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            PlanSeeder::class,
-            AdminSeeder::class,
+            // PlanSeeder::class,
+            // AdminSeeder::class,
         ]);
     }
 }
